f2444cc8a691b32db0b71317f5f3d0fbe89a6215
Hemakshi <hemakshi.jain@solutelabs.com>
2017-01-18 11:17:03 +0530

    Add ststicpages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
+  #get 'static_pages'
+
   # get 'static_pages/...'
 
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  get 'static_pages/home'
+#  get 'static_pages/home'
 
-  get 'static_pages/help'
+#  get 'static_pages/help'
 
   resources :microposts
   resources :users
