f22ae115548c4ad5c68191c5127331f32d915726
Hemakshi <hemakshi.jain@solutelabs.com>
2017-01-19 12:30:18 +0530

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
 
-  get 'static_pages/help'
+#  get 'static_pages/home'
+
+#  get 'static_pages/help'
+
+#  get 'static_pages/home'
+
+  #get 'static_pages/help'
 
   #get 'static_pages'
 
   # get 'static_pages/...'
 
   get 'static_pages/home'
-
   get 'static_pages/help'
-
+  get "static_pages/about"
 #  get 'static_pages/home'
 
 #  get 'static_pages/help'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,7 +20,17 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
   config.expect_with :rspec do |expectations|
+
+    require 'capybara'
+require 'capybara/dsl'
+
+  config.include Capybara::DSL
+
+
+
+
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
     # defined using `chain`, e.g.:
